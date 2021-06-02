7a865c5ecfe826cb00076c4b8f41b5fc8743f3dd
RGAC98 <59809286+RGAC98@users.noreply.github.com>
2021-06-02 12:20:14 -0500

    Feat: reconeccion a la base de heroku

--- config/cnn.js
+++ config/cnn.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg')
 
 const compras_bdd = new Pool({
-  host: 'localhost',
+  host: 'ec2-35-171-250-21.compute-1.amazonaws.com',
   port: '5432',
-  database: 'compras_bdd',
-  user: 'postgres',
-  password: '1459'
+  database: 'dbuadisl21lrlv',
+  user: 'gastqavwuzrgmi',
+  password: 'e95e874858f89be35db0c50ae5c8115ab0158cb9288b0db9f616d4870b2a00ee'
 })
 
 module.exports = {
