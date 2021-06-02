86c1cedf8a6cc40ff343829502cea4b329764e7e
RGAC98 <59809286+RGAC98@users.noreply.github.com>
2021-06-02 09:47:05 -0500

    fix: CRUDS factura_cabecera
    Se ha solucionado unos errores de la factura cabecera

--- controller/cabecera.controller.js
+++ controller/cabecera.controller.js
@@ -9,30 +9,30 @@ async function GetCabeceras(req, res)
 
 async function PostCreateCabecera(req, res)
 {
-    const fcab_prv_dni = req.body.fcab_prv_dni
+    const fcab_prv_id = req.body.fcab_prv_id
     const fcab_fecha_init = req.body.fcab_fecha_init
     const fcab_fecha_fin = req.body.fcab_fecha_fin
     const fcab_tipo_pago = req.body.fcab_tipo_pago
 
-    const cabecera = await cabecera_model.CreateCabecera(fcab_prv_dni, fcab_fecha_init, fcab_fecha_fin, fcab_tipo_pago)
+    const cabecera = await cabecera_model.CreateCabecera(fcab_prv_id, fcab_fecha_init, fcab_fecha_fin, fcab_tipo_pago)
     res.status(200).send({mensaje: cabecera})
 }
 
 async function UpdateCabecera(req, res)
 {
     const fcab_id = req.params.fcab_id
-    const fcab_prv_dni = req.body.fcab_prv_dni
+    const fcab_prv_id = req.body.fcab_prv_id
     const fcab_fecha_init = req.body.fcab_fecha_init
     const fcab_fecha_fin = req.body.fcab_fecha_fin
     const fcab_tipo_pago = req.body.fcab_tipo_pago
 
     try 
     {
-        const cabecera = await cabecera_model.UpdateCabecera(fcab_id, fcab_prv_dni, fcab_fecha_init, fcab_fecha_fin, fcab_tipo_pago)
+        const cabecera = await cabecera_model.UpdateCabecera(fcab_id, fcab_prv_id, fcab_fecha_init, fcab_fecha_fin, fcab_tipo_pago)
         res.status(200).send({mensaje: cabecera,
                               body: {
                                 fcab_id: fcab_id,
-                                fcab_prv_dni: fcab_prv_dni,
+                                fcab_prv_id: fcab_prv_id,
                                 fcab_fecha_init: fcab_fecha_init,
                                 fcab_fecha_fin: fcab_fecha_fin,
                                 fcab_tipo_pago: fcab_tipo_pago
--- model/factura_cabecera.model.js
+++ model/factura_cabecera.model.js
@@ -8,15 +8,15 @@ async function AllCabeceras() {
 }
 
 async function CreateCabecera(
-  fcab_prv_dni,
+  fcab_prv_id,
   fcab_fecha_init,
   fcab_fecha_fin,
   fcab_tipo_pago
 ) {
   // fcab_id, ID de la cabecera es SERIAL
-  const query = `INSERT INTO factura_cabecera(fcab_prv_dni, fcab_fecha_init, fcab_fecha_fin, fcab_tipo_pago) VALUES($1, $2, $3, $4)`;
+  const query = `INSERT INTO factura_cabecera(fcab_prv_id, fcab_fecha_init, fcab_fecha_fin, fcab_tipo_pago) VALUES($1, $2, $3, $4)`;
   const insert_cabecera = await compras_bdd.query(query, [
-    fcab_prv_dni,
+    fcab_prv_id,
     fcab_fecha_init,
     fcab_fecha_fin,
     fcab_tipo_pago,
@@ -26,17 +26,17 @@ async function CreateCabecera(
 
 async function UpdateCabecera(
   fcab_id,
-  fcab_prv_dni,
+  fcab_prv_id,
   fcab_fecha_init,
   fcab_fecha_fin,
   fcab_tipo_pago
 ) {
   const query = `UPDATE factura_cabecera
-	SET fcab_prv_dni=$2, fcab_fecha_init=$3, fcab_fecha_fin=$4, fcab_tipo_pago=$5
+	SET fcab_prv_id=$2, fcab_fecha_init=$3, fcab_fecha_fin=$4, fcab_tipo_pago=$5
 	WHERE fcab_id = $1`;
   const update_cabecera = await compras_bdd.query(query, [
     fcab_id,
-    fcab_prv_dni,
+    fcab_prv_id,
     fcab_fecha_init,
     fcab_fecha_fin,
     fcab_tipo_pago,
