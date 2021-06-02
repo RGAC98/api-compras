9495ca6b16b44eb6ca176655b4a930203aa5ca53
FernandoChulde <jfchuldeb@utn.edu.ec>
2021-06-02 13:18:02 -0500

    try catch

--- controller/cabecera.controller.js
+++ controller/cabecera.controller.js
@@ -1,57 +1,67 @@
 const { json } = require('express')
 const cabecera_model = require('../model/factura_cabecera.model')
 
-async function GetCabeceras(req, res)
-{
-    const cabeceras = await cabecera_model.AllCabeceras()
-    res.status(200).send({cabeceras: cabeceras})
-}
+async function GetCabeceras(req, res) {
+    try {
+        const cabeceras = await cabecera_model.AllCabeceras()
+        res.status(200).send({ cabeceras: cabeceras })
+    } catch (error) {
 
-async function PostCreateCabecera(req, res)
-{
-    const fcab_prv_id = req.body.fcab_prv_id
-    const fcab_fecha_init = req.body.fcab_fecha_init
-    const fcab_fecha_fin = req.body.fcab_fecha_fin
-    const fcab_tipo_pago = req.body.fcab_tipo_pago
-
-    const cabecera = await cabecera_model.CreateCabecera(fcab_prv_id, fcab_fecha_init, fcab_fecha_fin, fcab_tipo_pago)
-    res.status(200).send({mensaje: cabecera})
-}
-
-async function UpdateCabecera(req, res)
-{
-    const fcab_id = req.params.fcab_id
-    const fcab_prv_id = req.body.fcab_prv_id
-    const fcab_fecha_init = req.body.fcab_fecha_init
-    const fcab_fecha_fin = req.body.fcab_fecha_fin
-    const fcab_tipo_pago = req.body.fcab_tipo_pago
-
-    try 
-    {
-        const cabecera = await cabecera_model.UpdateCabecera(fcab_id, fcab_prv_id, fcab_fecha_init, fcab_fecha_fin, fcab_tipo_pago)
-        res.status(200).send({mensaje: cabecera,
-                              body: {
-                                fcab_id: fcab_id,
-                                fcab_prv_id: fcab_prv_id,
-                                fcab_fecha_init: fcab_fecha_init,
-                                fcab_fecha_fin: fcab_fecha_fin,
-                                fcab_tipo_pago: fcab_tipo_pago
-                                }
-                            })
-    } catch (error) 
-    {
-        res.status(500).json({error})    
     }
 
-    
 }
 
-async function DeleteCabecera(req, res)
-{
+async function PostCreateCabecera(req, res) {
+    const fcab_prv_id = req.body.fcab_prv_id
+    const fcab_fecha_init = req.body.fcab_fecha_init
+    const fcab_fecha_fin = req.body.fcab_fecha_fin
+    const fcab_tipo_pago = req.body.fcab_tipo_pago
+
+    try {
+        const cabecera = await cabecera_model.CreateCabecera(fcab_prv_id, fcab_fecha_init, fcab_fecha_fin, fcab_tipo_pago)
+        res.status(200).send({ mensaje: cabecera })
+    } catch (error) {
+
+    }
+
+}
+
+async function UpdateCabecera(req, res) {
+    const fcab_id = req.params.fcab_id
+    const fcab_prv_id = req.body.fcab_prv_id
+    const fcab_fecha_init = req.body.fcab_fecha_init
+    const fcab_fecha_fin = req.body.fcab_fecha_fin
+    const fcab_tipo_pago = req.body.fcab_tipo_pago
+
+    try {
+        const cabecera = await cabecera_model.UpdateCabecera(fcab_id, fcab_prv_id, fcab_fecha_init, fcab_fecha_fin, fcab_tipo_pago)
+        res.status(200).send({
+            mensaje: cabecera,
+            body: {
+                fcab_id: fcab_id,
+                fcab_prv_id: fcab_prv_id,
+                fcab_fecha_init: fcab_fecha_init,
+                fcab_fecha_fin: fcab_fecha_fin,
+                fcab_tipo_pago: fcab_tipo_pago
+            }
+        })
+    } catch (error) {
+        res.status(500).json({ error })
+    }
+
+
+}
+
+async function DeleteCabecera(req, res) {
     const fcab_id = req.params.fcab_id
 
-    const cabecera = await cabecera_model.DeleteCabecera(fcab_id)
-    res.status(200).send({mensaje: cabecera})
+    try {
+        const cabecera = await cabecera_model.DeleteCabecera(fcab_id)
+        res.status(200).send({ mensaje: cabecera })
+    } catch (error) {
+
+    }
+
 }
 
 module.exports = {
--- controller/proveedor.controller.js
+++ controller/proveedor.controller.js
@@ -1,62 +1,77 @@
 const { json } = require('express')
 const proveedor_model = require('../model/proveedor.model')
 
-async function GetProveedor(req, res)
-{
-    const proveedor = await proveedor_model.AllProveedor()
-    res.status(200).send({proveedores: proveedor})
-}
+async function GetProveedor(req, res) {
+    try {
+        const proveedor = await proveedor_model.AllProveedor()
+        res.status(200).send({ proveedores: proveedor })
+    } catch (error) {
 
-async function GetProveedorByDni(req, res)
-{   
-
-    const prv_dni = req.params.prv_dni
-    const proveedor = await proveedor_model.ProveedorByDni(prv_dni)
-    res.status(200).send({proveedores: proveedor})
-}
-
-async function PostCreateProveedor(req, res)
-{
-    const { prv_dni, prv_nombre, prv_ciudad, prv_tipo, prv_direccion, prv_telefono, prv_email, prv_estado } = req.body
-
-    const proveedor = await proveedor_model.CreateProveedor(prv_dni, prv_nombre, prv_ciudad, prv_tipo, prv_direccion, prv_telefono, prv_email, prv_estado)
-    res.status(200).send({mensaje: proveedor})
-}
-
-async function DELETEProveedor(req, res)
-{
-    const prv_id = req.params.prv_id
-
-    const proveedor = await proveedor_model.DeleteProveedor(prv_id)
-    res.status(200).send({mensaje: proveedor})
-}
-
-async function UPDATEProveedor(req, res)
-{
-    const { prv_id, prv_dni, prv_nombre, prv_ciudad, prv_tipo, prv_direccion, prv_telefono, prv_email, prv_estado } = req.body
-
-    try 
-    {
-        const proveedor = await proveedor_model.UpdateProveedor(prv_id, prv_dni, prv_nombre, prv_ciudad, prv_tipo, prv_direccion, prv_telefono, prv_email, prv_estado)
-        res.status(200).send({mensaje: proveedor,
-                              body: {
-                                prv_id: prv_id,
-                                prv_dni: prv_dni,
-                                prv_nombre: prv_nombre,
-                                prv_ciudad: prv_ciudad,
-                                prv_tipo: prv_tipo, 
-                                prv_direccion: prv_direccion, 
-                                prv_telefono: prv_telefono, 
-                                prv_email: prv_email, 
-                                prv_estado: prv_estado
-                                }
-                            })
-    } catch (error) 
-    {
-        res.status(500).json({error})    
     }
 
-    
+}
+
+async function GetProveedorByDni(req, res) {
+
+    const prv_dni = req.params.prv_dni
+
+    try {
+        const proveedor = await proveedor_model.ProveedorByDni(prv_dni)
+        res.status(200).send({ proveedores: proveedor })
+    } catch (error) {
+
+    }
+
+}
+
+async function PostCreateProveedor(req, res) {
+    const { prv_dni, prv_nombre, prv_ciudad, prv_tipo, prv_direccion, prv_telefono, prv_email, prv_estado } = req.body
+
+    try {
+        const proveedor = await proveedor_model.CreateProveedor(prv_dni, prv_nombre, prv_ciudad, prv_tipo, prv_direccion, prv_telefono, prv_email, prv_estado)
+        res.status(200).send({ mensaje: proveedor })
+    } catch (error) {
+
+    }
+
+}
+
+async function DELETEProveedor(req, res) {
+    const prv_id = req.params.prv_id
+
+    try {
+        const proveedor = await proveedor_model.DeleteProveedor(prv_id)
+        res.status(200).send({ mensaje: proveedor })
+    } catch (error) {
+
+    }
+
+}
+
+async function UPDATEProveedor(req, res) {
+    const { prv_id, prv_dni, prv_nombre, prv_ciudad, prv_tipo, prv_direccion, prv_telefono, prv_email, prv_estado } = req.body
+
+    try {
+        const proveedor = await proveedor_model.UpdateProveedor(prv_id, prv_dni, prv_nombre, prv_ciudad, prv_tipo, prv_direccion, prv_telefono, prv_email, prv_estado)
+        res.status(200).send({
+            mensaje: proveedor,
+            body: {
+                prv_id: prv_id,
+                prv_dni: prv_dni,
+                prv_nombre: prv_nombre,
+                prv_ciudad: prv_ciudad,
+                prv_tipo: prv_tipo,
+                prv_direccion: prv_direccion,
+                prv_telefono: prv_telefono,
+                prv_email: prv_email,
+                prv_estado: prv_estado
+            }
+        })
+    } catch (error) {
+        res.status(500).json({ error })
+    }
+
+
 }
 
 module.exports = {
